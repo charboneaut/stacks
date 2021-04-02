374f19f2c27ae82423e78e8e4a0efc607d20bce9
Timothy Charboneau <charboneau@protonmail.ch>
2021-04-02 12:55:21 -0400

    added pop to init and repr

--- stack.py
+++ stack.py
@@ -1,4 +1,9 @@
 class Stack():
-    def __init__(self):
-        """inits stack with an empty list"""
-        self.items = []
+    def __init__(self, items=[]):
+        """inits stack with content from 1st param,
+        otherwise empty"""
+        self.items = [*items]
+
+    def __repr__(self):
+        """prints items"""
+        print(self.items)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -6,3 +6,8 @@ class TestStacks(unittest.TestCase):
     def test_init(self):
         test_stack = Stack()
         self.assertIsInstance(test_stack, Stack)
+
+    def test_init_content(self):
+        test_stack = Stack([1, 2, 3])
+        self.assertIsInstance(test_stack, Stack)
+        self.assertEqual(test_stack.items, [1, 2, 3])
