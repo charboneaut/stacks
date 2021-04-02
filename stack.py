c7bb721bc82eaee46150ab1e020f25f4ac7cbada
Timothy Charboneau <charboneau@protonmail.ch>
2021-04-02 13:09:17 -0400

    added push and pop

--- stack.py
+++ stack.py
@@ -7,3 +7,11 @@ class Stack():
     def __repr__(self):
         """prints items"""
         print(self.items)
+
+    def push(self, items):
+        """adds items to end of items"""
+        self.items.append(items)
+    
+    def pop(self):
+        """removes the last item and returns it"""
+        return self.items.pop(len(self.items) - 1)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -11,3 +11,15 @@ class TestStacks(unittest.TestCase):
         test_stack = Stack([1, 2, 3])
         self.assertIsInstance(test_stack, Stack)
         self.assertEqual(test_stack.items, [1, 2, 3])
+
+    def test_push(self):
+        test_stack = Stack()
+        test_stack.push(1)
+        self.assertEqual(test_stack.items, [1])
+    
+    def test_pop(self):
+        test_stack = Stack([1, 2, 3])
+        test_return = test_stack.pop()
+        self.assertEqual(test_stack.items, [1, 2])
+        self.assertEqual(test_return, 3)
+        
